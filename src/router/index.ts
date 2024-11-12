67c22ea403eeb8d38fde08be681e24538dd8e6aa
cd1ng <81348194+cd1ng@users.noreply.github.com>
2024-11-13 07:57:08 +0800

    feat: project init

---
+++ src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+import Home from '@/views/home/index.vue'
+import NotFound from '@/views/notFound/index.vue'
+import User from '@/views/user/index.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/user', component: User },
+  { path: '/about', component: NotFound },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
---
+++ src/views/home/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/store/counter'
+// 不能对它进行解构，否则会报错
+const counter = useCounterStore()
+</script>
+
+<template>
+  Home
+  <button @click="counter.increment">+</button>
+  <p>{{ counter.count }}</p>
+  <p>doubleCount:{{ counter.doubleCount }}</p>
+</template>
