b39e9b0b81c35c4f919c0e20348ff359a73830b2
cd1ng <81348194+cd1ng@users.noreply.github.com>
2024-11-16 13:37:00 +0800

    fix: cannot jump modules routes

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -2,27 +2,27 @@ export default {
 	path: '/',
 	name: 'Home',
 	// 作为子路由，需要使用 Layout 组件包裹
-	component: () => import(/* webpackChunkName: "home" */ '@/layout/index.vue'),
+	component: () => import('@/layout/index.vue'),
 	children: [
 		{
 			path: '/',
 			name: 'HomePage',
-			component: () => import(/* webpackChunkName: "home" */ '@/views/home/index.vue')
+			component: () => import('@/views/home/index.vue')
 		},
 		{
 			path: '/user',
 			name: 'UserPage',
-			component: import(/* webpackChunkName: "user" */ '@/views/user/index.vue')
+			component: import('@/views/user/index.vue')
 		},
 		{
 			path: '/role',
 			name: 'RolePage',
-			component: import(/* webpackChunkName: "role" */ '@/views/role/index.vue')
+			component: import('@/views/role/index.vue')
 		},
 		{
 			path: '/list',
 			name: 'ListPage',
-			component: import(/* webpackChunkName: "list" */ '@/views/list/index.vue')
+			component: import('@/views/list/index.vue')
 		}
 	]
 }
